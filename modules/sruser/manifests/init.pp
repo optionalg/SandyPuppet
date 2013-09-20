9207512fb2e1e16b6f063d5c6ed1ee4d8bf2492b
Sandeep <sandeep.rawat@jasperindia.com>
2013-09-20 09:01:15 +0530

    Fixed module name in init.pp

--- modules/sruser/manifests/init.pp
+++ modules/sruser/manifests/init.pp
@@ -1,4 +1,4 @@
-define sdappuser($user,){  
+define sruser($user,){  
 
 	user { "${user}":
 		ensure     => 'present',
@@ -34,14 +34,14 @@ define sdappuser($user,){
                 owner => "${user}",
                 group => "${user}",
                 mode => '0640',
-		source =>  "puppet:///sdappuser/authorized_keys",
+		source =>  "puppet:///sruser/authorized_keys",
 		require => File["/home/${user}/.ssh"],
         }
 
 	
 	file { "/home/${user}/release/component-deploy.sh" :
 		ensure => present,
-                content   => template("sdappuser/scriptDir/component-deploy.sh"),
+                content   => template("sruser/scriptDir/component-deploy.sh"),
                 owner => "${user}",
                 group => "${user}",
                 mode   =>  '0740',
@@ -49,7 +49,7 @@ define sdappuser($user,){
 
 	file { "/home/${user}/release/component-war-deploy.sh" :
                 ensure => present,
-                content   => template("sdappuser/scriptDir/component-war-deploy.sh"),
+                content   => template("sruser/scriptDir/component-war-deploy.sh"),
                 owner => "${user}",
                 group => "${user}",
                 mode   =>  '0740',
@@ -57,7 +57,7 @@ define sdappuser($user,){
 
 	file { "/home/${user}/release/component-war-deploy-as-context.sh" :
                 ensure => present,
-                content   => template("sdappuser/scriptDir/component-war-deploy-as-context.sh"),
+                content   => template("sruser/scriptDir/component-war-deploy-as-context.sh"),
                 owner => "${user}",
                 group => "${user}",
                 mode   =>  '0740',
@@ -65,7 +65,7 @@ define sdappuser($user,){
 	
 	file { "/home/${user}/release/component-war-deploy-with-static.sh" :
                 ensure => present,
-                content   => template("sdappuser/scriptDir/component-war-deploy-with-static.sh"),
+                content   => template("sruser/scriptDir/component-war-deploy-with-static.sh"),
                 owner => "${user}",
                 group => "${user}",
                 mode   =>  '0740',
@@ -73,7 +73,7 @@ define sdappuser($user,){
 
 	file { "/home/${user}/release/startServer.sh" :
                 ensure => present,
-                content   => template("sdappuser/scriptDir/startServer.sh"),
+                content   => template("sruser/scriptDir/startServer.sh"),
                 owner => "${user}",
                 group => "${user}",
                 mode   =>  '0740',
@@ -81,7 +81,7 @@ define sdappuser($user,){
 
 	file { "/home/${user}/release/stopServer.sh" :
                 ensure => present,
-                content   => template("sdappuser/scriptDir/stopServer.sh"),
+                content   => template("sruser/scriptDir/stopServer.sh"),
                 owner => "${user}",
                 group => "${user}",
                 mode   =>  '0740',
@@ -89,7 +89,7 @@ define sdappuser($user,){
 
 	file { "/home/${user}/release/static-deploy.sh" :
                 ensure => present,
-                content   => template("sdappuser/scriptDir/static-deploy.sh"),
+                content   => template("sruser/scriptDir/static-deploy.sh"),
                 owner => "${user}",
                 group => "${user}",
                 mode   =>  '0740',
@@ -97,7 +97,7 @@ define sdappuser($user,){
 
         file { "/home/${user}/release/component-war-deploy-as-root.sh" :
                 ensure => present,
-                content   => template("sdappuser/scriptDir/component-war-deploy-as-root.sh"),
+                content   => template("sruser/scriptDir/component-war-deploy-as-root.sh"),
                 owner => "${user}",
                 group => "${user}",
                 mode   =>  '0740',
@@ -105,7 +105,7 @@ define sdappuser($user,){
 
 	file { "/home/${user}/release/restartServer.sh" :
                 ensure => present,
-                content   => template("sdappuser/scriptDir/restartServer.sh"),
+                content   => template("sruser/scriptDir/restartServer.sh"),
                 owner => "${user}",
                 group => "${user}",
                 mode   =>  '0740',
