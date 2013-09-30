073ff0d2b38b53883148d3814cff6607ff2cac6b
Sandeep Rawat <sandeep.rawat@jasperindia.com>
2013-09-30 21:53:11 +0530

    Added a solr core

---
+++ modules/solr-core-instance/manifests/init.pp
@@ -0,0 +1,24 @@
+define solr-core-instance( $instance_name, $core_name, $master, $slave, $port) {
+	file { "/etc/solr/${instance_name}/${core_name}/conf":
+		owner => solr,
+		group => solr,
+		ensure => directory,
+		source => "puppet:///modules/solr-core-instance/conf",
+		recurse => true,
+	}
+	
+	file { "/etc/solr/${instance_name}/${core_name}/conf/solrcore.properties":
+                owner => solr,
+                group => solr,
+                ensure => present,
+                content => template("solr-core-instance/solrcore.properties"),
+                recurse => true,
+        }
+
+	file { [ "/etc/solr/${instance_name}/${core_name}", "/etc/solr/${instance_name}/${core_name}/data" ]:
+                owner => solr,
+                group => solr,
+                ensure => directory,
+		before => File["/etc/solr/${instance_name}/${core_name}/conf"],
+        }
+}
