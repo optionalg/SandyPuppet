7db5890aeb485e33546d1c1eba3776b39a463e4c
Sandeep <sandeep.rawat@jasperindia.com>
2013-10-03 09:21:38 +0530

    Modified base mongo to follow standards

--- modules/base-mongo/manifests/init.pp
+++ modules/base-mongo/manifests/init.pp
@@ -1,64 +1,56 @@
 class base-mongo(){
 
 	$public_ip = $::ipaddress
+	include base-mongo::params, base-mongo::install, base-mongo::config,
+}
 
-		$server_pkg_name = $::osfamily ? {
-			debian => 'mongodb-10gen',
-			       redhat => 'mongo-10gen-server',
-			       default =>  'mongo-10gen-server',
+class base-mongo::params {
+	case $operatingsystem {
+		/(Ubuntu|Debian)/: {
+			$server_pkg_name = 'mongodb-10gen',
+			$sources_list_file = '/etc/apt/sources.list.d/10gen.list',
+			$sources_list_file_content = 'mongo_debian.repo',
 		}
-	$sources_list_file = $::osfamily ? {
-		debian => '/etc/apt/sources.list.d/10gen.list',
-		       redhat => '/etc/yum.repos.d/10gen.repo',
-		       default =>  '/etc/yum.repos.d/10gen.repo',
+		/(RedHat|CentOS|Fedora)/: {
+			$server_pkg_name = 'mongo-10gen-server',
+			$sources_list_file = '/etc/yum.repos.d/10gen.repo',
+			$sources_list_file_content = 'mongo.repo'
+		}
 	}
+}
 
-	$sources_list_file_content = $::osfamily ? {
-		debian => 'mongo_debian.repo',
-		       redhat => 'mongo.repo',
-		       default =>  'mongo.repo',
-	}
+class base-mongo::install {
 
-
-	file { "${sources_list_file}":
-		source => "puppet:///modules/base-mongo/${sources_list_file_content}",
-	}
-
-
-	user { 'mongo':
-		ensure => present,
-		       managehome => false,
-	}
-
-	file { 'data':
-		path => '/data',
-		     ensure => directory,
-	}
-	/*
-	   file { 'mongo_logs':
-	   path => '/var/log/mongo/',
-	   owner => mongo,
-	   group => mongo,
-	   ensure => directory,
-	   require => User['mongo'],
-	   before => Package['mongodb'],
-	   }
-	 */
+	
+	file { "$base-mongo::params::sources_list_file":
+                source => "puppet:///modules/base-mongo/$base-mongo::params::sources_list_file_content",
+        }
+	
 	package { 'mongodb':
-		name => "${server_pkg_name}",
-		     ensure => present,
-		     require => File["${sources_list_file}"],
+		name => "$base-mongo::params::server_pkg_name",
+		ensure => present,
+		require => File["$base-mongo::params::sources_list_file"],
 	}
-	file {  ["/data/mongo", "/var/log/mongo"] :
+
+}
+
+class base-mongo::config {
+
+        user { 'mongo':
+		ensure => present,
+		managehome => false,
+        }
+
+        file { '/data':
+		ensure => directory,
+        }
+        
+        file {  ["/data/mongo", "/var/log/mongo"] :
 		owner => mongo,
-		      group => mongo,
-		      ensure => directory,
-		      require => [ User['mongo'], File['data'], ],
-		      before => Package['mongodb'],
-		      recurse => true,
+		group => mongo,
+		ensure => directory,
+		require => [ User['mongo'], File['data'], ],
+		before => Class['base-mongo::install'],
 	}
-
-
-
 }
 
