4b101dae0f24034dc0d404baf85eaf062838086a
Sandeep Rawat <sandeep.rawat@jasperindia.com>
2013-10-13 20:02:33 +0530

    Added deployment functionality

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -8,3 +8,20 @@ class nodejs() {
 		ensure => directory,
 		require => Sruser['nodejs'],
 	}
+
+	file{ "/home/nodejs/deployNodeApp.sh":
+		owner => nodejs,
+		group => nodejs,
+		mode => '0750',
+		source => "puppet:///modules/nodejs/deployNodeApp.sh",
+		require => File["/home/nodejs/nodeapps"],
+	}
+
+	file{ "/home/nodejs/startNodeApp.sh":
+                owner => nodejs,
+                group => nodejs,
+                mode => '0750',
+                source => "puppet:///modules/nodejs/startNodeApp.sh",
+                require => File["/home/nodejs/nodeapps"],
+        }
+}
