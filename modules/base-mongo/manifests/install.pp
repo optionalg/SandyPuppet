be1479a885b129a86451b079ed8107e5dbfdbe2a
Sandeep Rawat <sandeep.rawat@jasperindia.com>
2013-10-15 22:56:21 +0530

    updated mongo install module to fix mongo isntallation issue

--- modules/base-mongo/manifests/install.pp
+++ modules/base-mongo/manifests/install.pp
@@ -4,7 +4,7 @@ class base-mongo::install {
 	file { "$base-mongo::params::sources_list_file":
                 source => "puppet:///modules/base-mongo/$base-mongo::params::sources_list_file_content",
         }
-	
+	#Sometime you have to execute the command sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10  to add key	
 	package { 'mongodb':
 		name => "$base-mongo::params::server_pkg_name",
 		ensure => present,
