dac350937ef48126b92b94cbca1dca76eb4f9c1e
Sandeep Rawat <sandeep.rawat@jasperindia.com>
2013-10-16 21:55:10 +0530

    Added monitrc

--- modules/monit/manifests/init.pp
+++ modules/monit/manifests/init.pp
@@ -2,4 +2,10 @@ class monit() {
 	package { 'monit':
 		ensure => present,
 	}
+
+	file { "/etc/monit/monitrc":
+                source => "puppet:///modules/monit/monitrc",
+		require => Package['monit'],
+        }
+	
 }
