1451124d3335bd845413cb5e23dc2c26037627dc
sandy <sandeep_r80@yahoo.co.in>
2013-10-22 11:31:15 +0530

    Modified tomcat module to add capability to change the base folder

--- modules/tomcat/manifests/init.pp
+++ modules/tomcat/manifests/init.pp
@@ -1,6 +1,6 @@
-define tomcat($user, $tomcat_location, $listening_port, $shutdown_port, $version = "7.0.40",){  
+define tomcat($user, $tomcat_location, $listening_port, $shutdown_port, $version = "7.0.40", $base_tomcat = "/usr/local"){  
 
-	file { "/usr/local/${tomcat_location}":
+	file { "${base_tomcat}/${tomcat_location}":
 		source  => "puppet:///modules/tomcat/apache-tomcat-${version}",
 		recurse => true,
 		owner => "${user}",
@@ -16,7 +16,7 @@ define tomcat($user, $tomcat_location, $listening_port, $shutdown_port, $version
                 mode => '0755',
         }
 	
-	file { "/usr/local/${tomcat_location}/conf/server.xml" :
+	file { "${base_tomcat}/${tomcat_location}/conf/server.xml" :
                      ensure  => present,
                      content => template("tomcat/server.xml.erb"),
                      require =>  File["/usr/local/${tomcat_location}"],
@@ -25,22 +25,12 @@ define tomcat($user, $tomcat_location, $listening_port, $shutdown_port, $version
                      group =>"${user}",
         }
 
-	file { "/usr/local/${tomcat_location}/webapps" :
+	file { "${base_tomcat}/${tomcat_location}/webapps" :
                 ensure => [ ['directory'] , ['present']],
                 require =>  File["/usr/local/${tomcat_location}"],
                 mode => '0750',
                 owner =>"${user}",
                 group =>"${user}",
-        }
-
-	file { "/usr/local/${tomcat_location}/webapps/ckeditor" :
-		ensure => [ ['directory'] , ['present']],
-		source  => "puppet:///modules/tomcat/ckeditor",
-		require =>  File["/usr/local/${tomcat_location}/webapps"],
-		mode => '0750',
-		owner =>"${user}",
-		group =>"${user}",
-		recurse => true,
         }
 }
 
