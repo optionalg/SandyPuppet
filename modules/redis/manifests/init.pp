46f05019bd2560b8579c5751ea5c5c0c74ba8d9f
sandy <sandeep_r80@yahoo.co.in>
2013-11-21 22:11:41 +0530

    Converted redis from class to module, now it can be used to create multiple redis instances

--- modules/redis/manifests/init.pp
+++ modules/redis/manifests/init.pp
@@ -1,13 +1,27 @@
-class redis() {
+define sr_redis($port = 6379) {
 
-	package { 'redis':
-		name => "redis-server",
-		ensure => present,
+	file { "/usr/local/bin/redis-server":
+		source  => "puppet:///modules/sr_redis/redis-server",
+		mode => 0754,
 	}
-	
-	file{ "/etc/init/redis.conf":
-                mode => '0750',
-                source => "puppet:///modules/redis/redis.conf",
-                require => Package["redis"],
+
+	file { "/usr/local/bin/redis-cli":
+                source  => "puppet:///modules/sr_redis/redis-cli",
+                mode => 0754,
         }
+
+
+	file {"/etc/redis/${port}.conf":
+		ensure => present,
+		content => template('sr_redis/redis.conf'),
+	}
+
+	file {"/etc/init.d/redis_${port}":
+                ensure => present,
+                content => template('sr_redis/redis_init_script'),
+        }
+
+	file {"/var/redis/${port}":
+		ensure => directory,
+	}
 }
