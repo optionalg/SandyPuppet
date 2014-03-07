03473f54c1507294ca047936223d3720c5b060ff
sandy <sandeep_r80@yahoo.co.in>
2014-03-07 22:12:08 +0530

    Updated base system configuration added curl

--- modules/base_system/manifests/init.pp
+++ modules/base_system/manifests/init.pp
@@ -2,10 +2,14 @@ class base_system() {
 	package { 'openjdk-7-jre':
 		ensure => present,
 	}
-	package { 'git-core':
+	package { 'git':
                 ensure => present,
         }
 	file { '/data':
 		ensure => directory,
         }
+	package { 'curl':
+                ensure => present,
+        }
+
 }
