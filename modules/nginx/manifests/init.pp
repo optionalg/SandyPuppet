27206e3367c4bd7e6fd991050dacc7c1ead220c6
sandy <sandeep_r80@yahoo.co.in>
2014-04-08 23:29:22 +0530

    Updated nginx installation for latest nginx

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,8 +1,27 @@
 class nginx() {
 
 	#########Refer this link in fase nginx.conf is not available http://askubuntu.com/questions/361902/how-to-install-nginx-after-removed-it-manually
+
+	Exec {
+                path => [
+                        '/usr/local/bin',
+                        '/opt/local/bin',
+                        '/usr/bin',
+                        '/usr/sbin',
+                        '/bin',
+                        '/sbin'],
+                        logoutput => true,
+        }
+
+	exec { "update_nginx":
+                command   => "add-apt-repository ppa:nginx/stable;apt-get update;apt-get upgrade",
+                logoutput => true,
+                timeout     => 1800,
+        }	
+
 	package { 'nginx':
 		ensure => present,
+		require => Exec['update_nginx'],
 	}
 
 	file {"/etc/nginx/ssl":
