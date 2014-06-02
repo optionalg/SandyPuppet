3a3409ffa528031ffa546084e13b5f0e2fb5ffee
sandy <sandeep_r80@yahoo.co.in>
2014-06-02 21:27:41 +0530

    Updated replicated single monog module for replica set initialization

--- modules/replicated_single_mongo/manifests/init.pp
+++ modules/replicated_single_mongo/manifests/init.pp
@@ -1,4 +1,4 @@
-class replicated_single_mongo($replicaSet="rs0"){
+define replicated_single_mongo($replicaSet="rs0", $host_name="127.0.0.1", $port="27017"){
 	Exec {
 		path => [
 			'/usr/local/bin',
@@ -15,10 +15,21 @@ class replicated_single_mongo($replicaSet="rs0"){
 		mode    => '644',
 	}
 
+	file { "/etc/master_replica":
+		content => template('replicated_single_mongo/master_replica'),
+	}
+
+
+
 	exec { "restart_replicated_mongo":
-		command => "service mongodb restart;mongo --eval 'rs.initiate()'; service mongodb restart",
+		command => "sleep 1m; service mongodb restart; sleep 1m",
 		logoutput => true,
 		require => File["/etc/mongodb.conf"],
 	}
 
+	exec { "register_replicaset_with_master":
+               command => "mongo ${host_name}:${port} < /etc/master_replica",
+               logoutput => true,
+               require => [File["/etc/master_replica"], Exec ["restart_replicated_mongo"]],
+       }
 }
