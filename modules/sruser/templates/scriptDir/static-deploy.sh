e7b5cf24147a5bfbd76d8c8733388317117751d5
sandy <sandeep_r80@yahoo.co.in>
2014-06-08 16:59:25 +0530

    Updated the script to deploy static code

--- modules/sruser/templates/scriptDir/static-deploy.sh
+++ modules/sruser/templates/scriptDir/static-deploy.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-RELEASE_DIR=$1
-STATIC_CONTENT_DIR=/var/www/lighttpd/static/$2
-echo "Copying content from $RELEASE_DIR/static/* to $STATIC_CONTENT_DIR"
-cp -r $RELEASE_DIR/static/* $STATIC_CONTENT_DIR;
 
+APP=$1
+CURRENT_TIME=`date +%y%m%d_%H%M%S`
+
+GREEN_STATIC_DIR=${HOME}/${APP}/static
+BLUE_STATIC_DIR=${HOME}/${APP}/${APP}-${CURRENT_TIME}
+
+mkdir ${BLUE_STATIC_DIR}
+echo "Copying the app content"
+tar -xzf ${HOME}/${APP}_static.tar.gz -C ${BLUE_STATIC_DIR}
+
+rm -f ${GREEN_STATIC_DIR}
+
+#Creating the link to newly downloaded code
+ln -s ${BLUE_STATIC_DIR} ${GREEN_STATIC_DIR}
+touch ${GREEN_STATIC_DIR}/latest
+
+source ${HOME}/release/file_functions.sh
+deleteOlderFiles ${HOME}/${APP} 4
