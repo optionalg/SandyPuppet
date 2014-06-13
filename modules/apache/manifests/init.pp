eed45b5a84c628b946a9cd8acca9b26d992b8cd9
sandy <sandeep_r80@yahoo.co.in>
2014-06-13 11:15:31 +0530

    Modified apache setup as Exec was not added

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -1,9 +1,21 @@
 class apache() {
+	Exec {
+		path => [
+			'/usr/local/bin',
+			'/opt/local/bin',
+			'/usr/bin',
+			'/usr/sbin',
+			'/bin',
+			'/sbin'],
+			logoutput => true,
+	}
+
 	package { 'apache2':
                 ensure => present,
         }
 
 	exec { 'install_default_modules':
 		command   => "a2enmod ssl proxy proxy_http"
+		require => Package['apache2'],
 	}
 }
